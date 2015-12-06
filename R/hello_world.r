9486f1cd45b6cd70064b1e52a52cdc28d3476af3
Jiří Moravec <j.moravec@massey.ac.nz>
2015-12-06 20:30:31 +1300

    initial commit

---
+++ R/hello_world.r
@@ -0,0 +1,10 @@
+#' Hello world
+#'
+#' Simple hello world function to test basic functionality.
+#' @keywords hello, world
+#' @export
+#' @examples
+#' hello_world()
+hello_world = function(){
+    print("Hello world")
+    }
